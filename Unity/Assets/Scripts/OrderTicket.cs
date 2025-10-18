7b992b5bed906365fd20359ff81f08db01c17df6
mayafederico <maya.federico8@gmail.com>
2025-10-17 17:44:46 -0700

    Started TicketUI
    made a prefab not sure if it's a good way to go about it though

---
+++ Unity/Assets/Scripts/OrderTicket.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OrderTicket : MonoBehaviour
+{
+    
+    // Sprites
+    public Sprite emptyCupHot;
+    public Sprite emptyCupIced;
+
+    public Sprite oneIce;
+    public Sprite twoIce;
+    public Sprite threeIce;
+
+    public Sprite regularMilk;
+    public Sprite oatMilk;
+    public Sprite almondMilk;
+
+    public Sprite chocolateSyrupPump;
+    public Sprite caramelSyrupPump;
+    public Sprite mochaSyrupPump;
+    
+    public Sprite whippedCream;
+    public Sprite chocolateSyrup;
+    public Sprite caramelSyrup;
+    
+    // GameObjects
+    public GameObject DrinkType;
+    public GameObject milkType;
+    public GameObject syrupType;
+    public GameObject whippedCreamTopping;
+    public GameObject chocolateSyrupTopping;
+    public GameObject caramelSyrupTopping;
+    
+    /*
+    // ChatGpt
+    [Header("UI")] 
+    [SerializeField] private Text orderNumberText;
+    [SerializeField] private Text drinkNameText;
+
+    [Header("Milk Type")] 
+    //[SerializeField] private GameObject milkType;
+    
+    [NonSerialized] public string drinkName;
+
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
---
+++ Unity/Assets/Scripts/TicketBoard.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class TicketBoard : MonoBehaviour
+{
+    /*
+    // All ChatGPT
+    
+    [Header("Prefab & Parent")]
+    [SerializeField] private OrderTicket ticketPrefab;
+    [SerializeField] private RectTransform ticketsParent;
+
+    public OrderTicket SpawnTicket(string drinkName, Sprite drinkIcon)
+    {
+        if (!ticketPrefab || !ticketsParent) return null;
+        
+        var ticket = Instantiate(ticketPrefab, ticketsParent);
+        ticket.transform.SetAsLastSibling();
+
+        ticket.Setup(drinkName, drinkIcon);
+        
+        return ticket;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
